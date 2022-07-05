a9eec81a527a7301b7695db9f973f8224f8f497e
nemaxim84 <memaxim@yandex.ru>
2022-07-05 13:00:10 +0300

    first commit

---
+++ src/main/java/com/example/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        /*
+             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+         */
+        boolean checkNameToEmboss;
+        if (name.contains(" ") && name.length() >= 3 && name.length() <= 19 && !name.endsWith(" ") && !name.startsWith(" ") && name.split(" ").length == 2) {
+            return true;
+        } else return false;
+    }
+}
